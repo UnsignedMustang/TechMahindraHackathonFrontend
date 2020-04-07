b8959ad058bd19394ef493d3f1a9e2919f4c405e
JOYDEEP BISWAS <mr.joydeepbiswas@gmail.com>
2020-04-07 12:56:05 +0530

    new component login-screen added changes on app.component.html

--- src/app/login-screen/login-screen.component.ts
+++ src/app/login-screen/login-screen.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginScreenComponent implements OnInit {
 
-  constructor() { }
+
+  constructor() {}
 
   ngOnInit() {
+
   }
 
 }
+
